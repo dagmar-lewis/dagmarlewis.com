e3f26e3cde000c8e184e9c6d8e17c8da6eb26598
Dagmar Lewis <dagmarlewis8@gmail.com>
2025-07-15 19:30:01 -0500

    change command

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "cloudfront_url" {
-  value = aws_cloudfront_distribution.vpc_origin_distribution.domain_name
-}
+# output "cloudfront_url" {
+#   value = aws_cloudfront_distribution.vpc_origin_distribution.domain_name
+# }
 
 
 output "ec2id" {
