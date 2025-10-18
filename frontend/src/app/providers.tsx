2b206129c314506eb35b679964b284d5effd5baf
Dagmar Lewis <dagmarlewis8@gmail.com>
2025-10-18 14:41:30 -0500

    add posthog as provider

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -8,7 +8,7 @@
         posthog.init('phc_KQNV8w6qKbrE5qfwyLV9lCQR2bbMB921WQPqB5WXsx9', {
             api_host: 'https://us.i.posthog.com',
             defaults: '2025-05-24',
-            person_profiles: 'always'
+            cookieless_mode: 'always'
         })
     }
 
